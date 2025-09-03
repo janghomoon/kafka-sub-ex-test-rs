797c4e5fc2b65017d7f5514bb60e2be179d0a5bc
janghomoon <sonyjangho@gmail.com>
2025-09-03 15:36:35 +0900

    Initial commit

---
+++ src/main/java/kr/co/kafkasubextestrs/KafkaSubExTestRsApplication.java
@@ -0,0 +1,13 @@
+package kr.co.kafkasubextestrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSubExTestRsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaSubExTestRsApplication.class, args);
+    }
+
+}
