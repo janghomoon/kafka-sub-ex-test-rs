ecd4e3bfa967df00dc6be4309f98d425e544e9a6
janghomoon <sonyjangho@gmail.com>
2025-09-05 23:13:31 +0900

    feat - config 추가

---
+++ src/main/java/kr/co/kafkasubextestrs/cofig/KafkaConsumerConfig.java
@@ -0,0 +1,9 @@
+package kr.co.kafkasubextestrs.cofig;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@Configuration
+@EnableKafka
+public class KafkaConsumerConfig {
+}
